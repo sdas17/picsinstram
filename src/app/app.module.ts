48dcc4183ba39faab4f8bb9f2cd515771eee26c2
sdas1 <sdas1@miraclesoft.com>
2022-10-26 20:14:52 +0530

    add commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hellow.component';
+import { ByeComponent } from './bye/bye.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    ByeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hellow.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template:`<p>Hello my name is subhham kumar das</p>`
+})
+
+export class HelloComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
+
